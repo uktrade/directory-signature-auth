8e287208ddc73d84f7e69b09d4a7b0588d371d19
Rollo Konig-Brock <rollokb@gmail.com>
2018-06-08 14:31:34 +0100

    Update setup py to work with pip10

--- setup.py
+++ setup.py
@@ -17,13 +17,6 @@ def get_version():
         ))
 
 
-def get_requirements():
-    return [str(r.req) for r in list(parse_requirements(
-        'requirements.txt',
-        session=pip.download.PipSession()
-    ))]
-
-
 setup(
     name='sigauth',
     version=get_version(),
@@ -34,7 +27,11 @@ setup(
     packages=find_packages(exclude=["tests.*", "tests"]),
     long_description=open('README.md').read(),
     include_package_data=True,
-    install_requires=get_requirements(),
+    install_requires=[
+        'django>=1.9,<2.0a1',
+        'djangorestframework>=3.4.7,<4.0.0',
+        'mohawk>=0.3.4,<1.0.0',
+    ],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
